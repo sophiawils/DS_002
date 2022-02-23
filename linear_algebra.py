c6c77adde54e17b2bf18faf59aabd6c7c0c72b9f
sophiawils <98052671+sophiawils@users.noreply.github.com>
2022-02-23 10:06:30 -0800

    added lin alg functions
    should have all functions from prof. goodwin's colab linear algebra template

--- linear_algebra.py
+++ linear_algebra.py
@@ -67,8 +67,30 @@ def sum_of_squares(v: Vector) -> float:
     """Returns v_1 * v_1 + ... + v_n * v_n"""
     return dot(v, v)
 
+
+import math
+
+def magnitude(v: Vector) -> float:
+    """Returns the magnitude (or length) of v"""
+    return math.sqrt(sum_of_squares(v))   # math.sqrt is square root function
+
+
+def squared_distance(v: Vector, w: Vector) -> float:
+    """Computes (v_1 - w_1) ** 2 + ... + (v_n - w_n) ** 2"""
+    return sum_of_squares(subtract(v, w))
+
+# def distance(v: Vector, w: Vector) -> float:
+#     """Computes the distance between v and w"""
+#     return math.sqrt(squared_distance(v, w))
+
+# This is possibly clearer if we write it as (the equivalent):
+def distance(v: Vector, w: Vector) -> float:  # type: ignore
+    return magnitude(subtract(v, w))
+
 assert add([1, 2, 3], [10,9,8]) == [11,11,11], "something wrong with add()"
 assert subtract([11,11,11], [1, 2, 3]) == [10,9,8], "trouble with subtract()"
 assert vector_sum([[3,4], [5,6], [7,8]]) == [15, 18], "vector_sum() problem"
 assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4], "oopsie vector_mean()"
 assert dot([1, 2, 3], [4, 5, 6]) == 32, "dot() issue"  # 1 * 4 + 2 * 5 + 3 * 6
+assert sum_of_squares([1, 2, 3]) == 14, "sum_of_squares() fail"  # 1 * 1 + 2 * 2 + 3 * 3
+assert magnitude([3, 4]) == 5, "issue with magnitude()"
