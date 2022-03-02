d4d157e98780dc4aef6b451ffd544a33b8690416
sophiawils <98052671+sophiawils@users.noreply.github.com>
2022-03-01 23:01:25 -0800

    added more lin alg functions and assert statements

--- linear_algebra.py
+++ linear_algebra.py
@@ -87,6 +87,57 @@ def squared_distance(v: Vector, w: Vector) -> float:
 def distance(v: Vector, w: Vector) -> float:  # type: ignore
     return magnitude(subtract(v, w))
 
+###matrices
+
+# Another type alias
+Matrix = List[List[float]]
+
+A = [[1, 2, 3], # A has 2 rows and 3 columns
+    [4, 5, 6]]
+""
+B = [[1, 2], # B has 3 rows and 2 columns
+     [3, 4],
+     [5, 6]]
+
+from typing import Tuple
+
+def shape(A: Matrix) -> Tuple[int, int]:
+  """Returns (# of rows of A, # of columns of A)"""
+  num_rows = len(A)
+  num_cols = len(A[0]) if A else 0 # number of elements in first row 
+  return num_rows, num_cols
+
+# If a matrix has n rows and k columns, we will refer to it as an n × k matrix. 
+# We can (and sometimes will) think of each row of an n × k matrix as a vector 
+# of length k, and each column as a vector of length n:
+
+def get_row(A: Matrix, i: int) -> Vector:
+  """Returns the i-th row of A (as a Vector)"""
+  return A[i] # A[i] is already the ith row
+
+def get_column(A: Matrix, j: int) -> Vector: 
+  """Returns the j-th column of A (as a Vector)""" 
+  return [A_i[j] for A_i in A] # jth element of row A_i
+  # for A_i in A] # for each row A_i
+    
+from typing import Callable
+def make_matrix(num_rows: int,
+                num_cols: int,
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """
+    Returns a num_rows x num_cols matrix
+    whose (i,j)-th entry is entry_fn(i,j)
+    """
+    return [[entry_fn(i,j)              # given i, create a list
+             for j in range(num_cols)]  # [entry_fn(i,0),...]
+            for i in range(num_rows)]   # create one list for each i
+
+def identity_matrix(n: int) -> Matrix:
+    """Returns the n x n identity matrix"""
+    return make_matrix(n,n, lambda i,j: 1 if i == j else 0)
+
+
+
 assert add([1, 2, 3], [10,9,8]) == [11,11,11], "something wrong with add()"
 assert subtract([11,11,11], [1, 2, 3]) == [10,9,8], "trouble with subtract()"
 assert vector_sum([[3,4], [5,6], [7,8]]) == [15, 18], "vector_sum() problem"
@@ -94,3 +145,31 @@ assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4], "oopsie vector_mean()"
 assert dot([1, 2, 3], [4, 5, 6]) == 32, "dot() issue"  # 1 * 4 + 2 * 5 + 3 * 6
 assert sum_of_squares([1, 2, 3]) == 14, "sum_of_squares() fail"  # 1 * 1 + 2 * 2 + 3 * 3
 assert magnitude([3, 4]) == 5, "issue with magnitude()"
+
+assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
+assert subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
+assert vector_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20]
+assert scalar_multiply(2, [1, 2, 3]) == [2, 4, 6]
+assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4]
+assert dot([1, 2, 3], [4, 5, 6]) == 32  # 1 * 4 + 2 * 5 + 3 * 6
+assert sum_of_squares([1, 2, 3]) == 14  # 1 * 1 + 2 * 2 + 3 * 3
+assert magnitude([3, 4]) == 5
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)  # 2 rows, 3 columns
+assert distance([1,1],[4,1]) == 3.0
+assert squared_distance([1,2,3],[2,3,4]) == 3
+assert scalar_multiply(2, [1,2,3]) == [2,4,6]
+assert magnitude([0,0,4,3]) == 5.0
+
+# Work on an Identity Matrix
+id = [  [1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1] ]
+
+assert get_column(id,2) == [0, 0, 1, 0, 0]
+assert get_row(id,2) == [0, 0, 1, 0, 0]
+assert get_column(id,2) == get_row(id,2)
+assert identity_matrix(5) == id
+assert make_matrix(5,5, lambda i,j: 1 if i == j else 0) == id
+assert shape(id) == (5,5)
